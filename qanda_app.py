60f091fe14b39108219d94ecaa219fd0800a665b
Danijel Petkovic <danijel.petkovic@neta.io>
2021-11-10 19:06:22 +0100

    Add and style the answer placeholder

--- qanda_app.py
+++ qanda_app.py
@@ -1,35 +1,50 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-
-st.title('Ask an interesting question')
-
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
-@st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
-
-data = load_data(10000)
-
-
-
-st.text_input('Enter a question')
-
-st.checkbox('Enable voice')
-
-confirm_button = st.button('Get an answer')
 
 def main():
-    if confirm_button:
-        st.spinner("Generating an answer...")
+    st.set_page_config(
+        page_title="Qanda app",
+        layout="wide",
+        initial_sidebar_state="expanded"
+    )
 
-st.text('This is an answer')
+    st.title('Ask an interesting question')
+
+    question = st.text_input('Enter a question')
+
+    voice = st.checkbox('Enable voice')
+
+    confirm_button = st.button('Get an answer')
+
+    st.markdown(
+        "<hr />",
+        unsafe_allow_html=True
+    )
+
+    if confirm_button:
+        with st.spinner("Generating recipe..."):
+            if question:
+                generated_answer = 'This is the answer'
+
+                st.markdown(
+                    " ".join([
+                        "<div>",
+                        "<h2 class='font-title text-bold'>The answer:</h2>",
+                        '<div style="padding: 30px;background-color: #B6C9B1; border-radius: 10px;">',
+                        f'<p>{generated_answer}</p>',
+                        "</div>",
+                        "</div>"
+                    ]),
+                    unsafe_allow_html=True
+                )
+
+                if voice:
+                    st.markdown(
+                        " ".join([
+                            "<div style='padding: 20px;background-color: #A7BFC7; border-radius: 10px;margin-top: 30px;'>",
+                            '<p>Audio Coming soon...</p>',
+                            "</div>"
+                        ]),
+                        unsafe_allow_html=True
+                    )
 
 main()
